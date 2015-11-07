c5a0ffa67f563260d62df0590add89a7dbe3e7f8
justin <jmwagg@gmail.com>
2015-11-07 12:46:36 -0500

    Added plot.ly
    Instead of printing to CSV file, we can use plotly to plot the contents
    of the randomly generated value array.

--- data_writer.py
+++ data_writer.py
@@ -2,15 +2,25 @@
 import random
 import pprint as p
 import csv
+import numpy
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 squares = []
 
-#append 1000 random values with mean of 1 and stdev of .2
+#append 1000 random values random.gauss(mean,stdev)
 for i in range(1000):
-	squares.append(random.gauss(1,.2))
+	squares.append(random.gauss(100,50))
 
 #open and write values to a csv file
-with open('test2.csv' , 'w') as csvfile:
-	test = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	for i in range(len(squares)):
-		test.writerow([squares[i]])
+# with open('test2.csv' , 'w') as csvfile:
+# 	test = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+# 	for i in range(len(squares)):
+# 		test.writerow([squares[i]])
+
+# hist, bin_edges = numpy.histogram(squares, bins = 100)
+
+# p.pprint(hist)
+
+data = [go.Histogram(x = squares)]
+plot_url = py.plot(data, filename = 'histogram')
